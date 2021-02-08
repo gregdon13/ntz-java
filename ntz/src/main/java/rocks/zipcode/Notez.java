59d482b45175c25700d0982fc094afe6f190dffe
Kristofer <kris@youngers.org>
2021-02-08 16:00:49 -0500

    more in Notez

--- ntz/src/main/java/rocks/zipcode/Notez.java
+++ ntz/src/main/java/rocks/zipcode/Notez.java
@@ -5,6 +5,11 @@ package rocks.zipcode;
  */
 public final class Notez {
 
+    private FileMap filemap;
+
+    public Notez() {
+        this.filemap  = new FileMap();
+    }
     /**
      * Says hello to the world.
      * 
@@ -21,17 +26,51 @@ public final class Notez {
             System.err.println("]");
         }
 
-        FileMap filemap = new FileMap();
+        Notez ntzEngine = new Notez();
+
+        ntzEngine.loadDatabase();
+
+        /*
+         * You will spend a lot of time right here.
+         * 
+         * instead of loadDemoEntries, you will implement a series
+         * of method calls that manipulate the Notez engine.
+         * See the first one:
+         */
+        ntzEngine.loadDemoEntries();
+
+        ntzEngine.saveDatabase();
+
+        if (argv.length == 0) { // there are no commandline arguments
+            //just print the contents of the filemap.
+            ntzEngine.printResults();
+        }
+        /*
+         * what other method calls do you need here to implement the other commands??
+         */
+
+    }
+
+    private void saveDatabase() {
+        filemap.save();
+    }
+
+    private void loadDatabase() {
         filemap.load();
+    }
+
+    public void printResults() {
+        System.out.println(this.filemap.toString());
+    }
+
+    public void loadDemoEntries() {
         filemap.put("General", new NoteList("The Very first Note"));
         filemap.put("note2", new NoteList("A secret second note"));
         filemap.put("category3", new NoteList("Did you buy bread AND eggs?"));
         filemap.put("anotherNote", new NoteList("Hello from ZipCode!"));
-        filemap.save();
-
-        if (argv.length == 0) {
-            System.out.println(filemap.toString());
-        }
-
     }
+    /*
+     * Put all your additional methods that implement commands like forget here...
+     */
+    
 }
